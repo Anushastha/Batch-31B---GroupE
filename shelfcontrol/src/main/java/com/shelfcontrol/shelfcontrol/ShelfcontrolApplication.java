4d6f60ac82ed74330f5cdc2b1f5859dc3a4f9282
sarinamagar <msarina823@gmail.com>
2022-12-05 11:15:30 +0545

    initilize

---
+++ shelfcontrol/src/main/java/com/shelfcontrol/shelfcontrol/ShelfcontrolApplication.java
@@ -0,0 +1,13 @@
+package com.shelfcontrol.shelfcontrol;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShelfcontrolApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShelfcontrolApplication.class, args);
+	}
+
+}
---
+++ shelfcontrol/src/test/java/com/shelfcontrol/shelfcontrol/ShelfcontrolApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shelfcontrol.shelfcontrol;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShelfcontrolApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
